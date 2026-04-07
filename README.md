313b19cd30e0155ecb34ceea39b664562952591f
Angel T. Saez <angel.tsr.00@gmail.com>
2026-04-07 10:35:54 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,203 @@
+Structural learning of tree aumented naive Bayes using mixtures of
+polynomials
+================
+
+This bundle contains the manuscript submitted to the Kybernetika and
+entitled “*Structural learning of tree aumented naive Bayes using
+mixtures of polynomial*”. The organisation is the following:
+
+- fitTANGAUSS.R: R file where the code for learning conditional Gaussian
+  TAN.
+- example.R: A R file with a toy example for fit a conditional Gaussian
+  TAN model.
+
+The *fitTANGauss.R* file contains the `fit_tan_g`function. It computes
+the structural and the parameters of a TAN models given a data.frame
+considering the conditional Gaussian assumptions. In addition, this file
+has the following auxiliary functions:
+
+- `cond_mi_cont` and `cond_mi_cont_disc`. They compute the mutual
+  information of two predicted variables given the class using
+  conditional Gaussian assuptions.
+- `MI_tan_gauss`. It computes the mutual information between each pair
+  of feature variables given the class variable.
+- `fit_root`. It computes a possible root for TAN model.
+- `fit_tan_structure`.It computes the structure of a TAN model
+  considering the conditional Gaussian assumptions given a mutual
+  information matrix computed by `MI_tan_gauss`.
+
+## Packages needed for running the code
+
+The R packages needed for running the toy example could be downloaded
+using Cran Repository. Using the following R commands to install the
+packages:
+
+``` r
+# install.packages("bnlearn")
+# install.packages("MASS")
+# install.packages("infotheo")
+```
+
+## Data
+
+The toy problem consists in a synthetic dataset with a class variable
+*C*, two continuous features random variables and two discrete features
+random variables following the conditional Gaussian assumptions. The
+dataset contains 100 instances.
+
+The following code load the toy problem dataset to the environment.
+
+``` r
+load("data.Rda")
+```
+
+## Computing conditional Gaussian TAN
+
+First, we source the *fitTANGauss.R* file for load the neccessary
+functions to compute the conditional Gaussian TAN model.
+
+``` r
+# Source fitTANGauss.R file
+source("fitTANGauss.R")
+```
+
+Now, the conditional Gaussian TAN model can be computed using
+`fit_tan_g`. This function requires *bnlearn* package for the parameter
+learning.
+
+``` r
+# bnlearn R package is required for parameter learning process
+library(bnlearn)
+tan = fit_tan_g(target = "C",data = data)
+tan
+```
+
+    ## 
+    ##   Bayesian network parameters
+    ## 
+    ##   Parameters of node C (multinomial distribution)
+    ## 
+    ## Conditional probability table:
+    ##     0    1 
+    ## 0.61 0.39 
+    ## 
+    ##   Parameters of node Z1 (conditional Gaussian distribution)
+    ## 
+    ## Conditional density: Z1 | C + Y2
+    ## Coefficients:
+    ##                       0           1           2           3
+    ## (Intercept)   0.5326947   0.5497585  -0.5878010  -0.6413849
+    ## Standard deviation of the residuals:
+    ##         0          1          2          3  
+    ## 0.9508448  0.9166042  0.6780353  0.6617743  
+    ## Discrete parents' configurations:
+    ##    C  Y2
+    ## 0  0   0
+    ## 1  1   0
+    ## 2  0   1
+    ## 3  1   1
+    ## 
+    ##   Parameters of node Z2 (conditional Gaussian distribution)
+    ## 
+    ## Conditional density: Z2 | C + Z1
+    ## Coefficients:
+    ##                0    1
+    ## (Intercept)  2.0  3.0
+    ## Z1           0.9  0.8
+    ## Standard deviation of the residuals:
+    ##         0          1  
+    ## 0.4395684  0.6080541  
+    ## Discrete parents' configurations:
+    ##    C
+    ## 0  0
+    ## 1  1
+    ## 
+    ##   Parameters of node Y1 (multinomial distribution)
+    ## 
+    ## Conditional probability table:
+    ##  
+    ## , , Y2 = 0
+    ## 
+    ##    C
+    ## Y1          0         1
+    ##   0 0.3125000 0.4285714
+    ##   1 0.6875000 0.5714286
+    ## 
+    ## , , Y2 = 1
+    ## 
+    ##    C
+    ## Y1          0         1
+    ##   0 0.5862069 0.5555556
+    ##   1 0.4137931 0.4444444
+    ## 
+    ## 
+    ##   Parameters of node Y2 (multinomial distribution)
+    ## 
+    ## Conditional probability table:
+    ##  
+    ##    C
+    ## Y2          0         1
+    ##   0 0.5245902 0.5384615
+    ##   1 0.4754098 0.4615385
+
+``` r
+# Plot the DAG using the graphviz.plot() from bnlearn package
+graphviz.plot(tan)
+```
+
+    ## Loading required namespace: Rgraphviz
+
+![](Readme_files/figure-gfm/unnamed-chunk-4-1.png)<!-- -->
+
+### Choosing the root of TAN
+
+The function `fit_tan_g` allows introduces the MI matrix and the root.
+By default, the root is chosen as one of the feature variable with the
+link with highest value in the MI matrix. If there are discrete and
+continuous variables, the root is chosen considering only discrete
+variables.
+
+We can compute the conditional mutual information before TAN using
+conditional Gaussian assumptions using `MI_tan_gauss`.
+
+``` r
+MI = MI_tan_gauss(data = data,target = "C")
+MI
+```
+
+    ##            Z1        Z2         Y1         Y2
+    ## Z1 0.00000000 0.7057450 0.07103087 0.22140686
+    ## Z2 0.70574501 0.0000000 0.14934397 0.16443592
+    ## Y1 0.07103087 0.1493440 0.00000000 0.02650156
+    ## Y2 0.22140686 0.1644359 0.02650156 0.00000000
+
+And, the TAN model using *Y1* as root is obtained with the following
+code
+
+``` r
+# TAN with Y1 as root
+tan2 = fit_tan_g(target = "C",data = data,root = "Y1",mutualInfoCond = MI)
+graphviz.plot(tan2)
+```
+
+![](Readme_files/figure-gfm/unnamed-chunk-6-1.png)<!-- -->
+
+``` r
+# TAN model with illegal root
+tan3 = fit_tan_g(target = "C",data = data,root = "Z1")
+graphviz.plot(tan3)
+```
+
+![](Readme_files/figure-gfm/unnamed-chunk-6-2.png)<!-- -->
+
+Finally, when there are only continuous feature variables.
+
+``` r
+# TAN model with only continuous feature variables
+tan4 = fit_tan_g(target = "C",data = data[,c("C","Z1","Z2")],root = "Z1")
+graphviz.plot(tan4)
+```
+
+![](Readme_files/figure-gfm/unnamed-chunk-7-1.png)<!-- -->
+
+\`\`\`
--- fitTANGauss.R
+++ fitTANGauss.R
@@ -1,62 +1,56 @@
-# Funciones para crear el TAN
-
-# Función para el algoritmo de chow-liu--------------------
-## Función para el arbol maximal--------------------
-prim_maximal_g <- function(adj_mat,root = 1,pred_disc = character()) {
-  # Número de nodos en el grafo
-  # browser()
-  n <- nrow(adj_mat)
+# Conditional Gaussian TAN
+# Chow-Liu Algorithm--------------------
+## Compute TAN structure----------------------------
+fit_tan_structure = function(mutualInfoCond,target,root=1,pred_disc){
+  # Compute maximun spanning tree
+  
+  # Number of nodes
+  n <- nrow(mutualInfoCond)
   if(is.null(n)){
-    n=1# caso 1 variable
+    n=1
   }
-  
-  # Inicialización
+  # Inicializate
   selected_nodes <- rep(FALSE, n)
-  names(selected_nodes) = row.names(adj_mat)
-  selected_nodes[root] <- TRUE  # Seleccionamos el nodo inicial arbitrario
-  maximal_tree <- matrix(0, n, n,dimnames = dimnames(adj_mat))  # Para almacenar el árbol máximo
+  names(selected_nodes) = row.names(mutualInfoCond)
+  # root of maximal tree
+  selected_nodes[root] <- TRUE  
+  # Adjacency matrix of tree
+  maximal_tree <- matrix(0, n, n,dimnames = dimnames(mutualInfoCond))  
   
-  # Bucle principal
+  # Compute maximal spanning tree
   i = 1
   for (i in 1:(n-1)) {
     max_weight <- -Inf
-    u <- -1#Fila
-    v <- -1# Columna
+    u <- -1#row
+    v <- -1# Column
     if(all(selected_nodes[pred_disc])){
+      # All discrete nodes are selected
       option_nodes = names(which(!selected_nodes))
     }else{
+      # There is not discrete nodes selected
       option_nodes = names(which(!selected_nodes[pred_disc]))
     }
-    # Encontrar la arista de mayor peso que conecta un nodo seleccionado con uno no seleccionado
+    # Find the node which is not selected with better value
     for (j in names(which(selected_nodes))) {
       for (k in option_nodes) {
-        if (adj_mat[j, k] > max_weight) {
-          max_weight <- adj_mat[j, k]
+        if (mutualInfoCond[j, k] > max_weight) {
+          max_weight <- mutualInfoCond[j, k]
           u <- j
           v <- k
         }
       }
     }
     
-    # Añadir la arista al árbol maximal
+    # Add the node to the tree
     if (u != -1 && v != -1) {
       maximal_tree[u, v] <- 1
-      # maximal_tree[v, u] <- max_weight
       selected_nodes[v] <- TRUE
     }
   }
-  return(maximal_tree)
-}
-
-## Funcion para generar la estructura TAN----------------------------
-fit_tan_structure = function(mutualInfoCond,target,root=1,pred_disc){
-  # browser()
-  # Matriz de adjacencia
-  adj_mat <- prim_maximal_g(mutualInfoCond,root = root)
-  # nombre de las variables
-  varTAN <- c(row.names(adj_mat),target)
-  # anadimos la clase
-  adj_mat <- rbind(adj_mat,1)# Aristas del target a todos los predictores
+  # variable names
+  varTAN <- c(row.names(maximal_tree),target)
+  # Adding the class to the tree as father
+  adj_mat <- rbind(maximal_tree,1)
   adj_mat <- cbind(adj_mat,0)
   rownames(adj_mat)<- varTAN
   colnames(adj_mat)<- varTAN
@@ -64,9 +58,11 @@ fit_tan_structure = function(mutualInfoCond,target,root=1,pred_disc){
   return(adj_mat)
 }
 
-# funcion para calcular la raiz de un TAN---------------------
+# choose root---------------------
 fit_root = function(MI,pred_disc = character()){
+  
   if(length(pred_disc)!=0){
+    # If there are discrete feature variabes, one of them must be the root
     MI = MI[pred_disc,pred_disc,drop = F]
   }
   root = which.max(MI)%%nrow(MI)
@@ -76,8 +72,8 @@ fit_root = function(MI,pred_disc = character()){
   return(root)
 }
 
-# Funcion para aprender un tan discreto CG--------------------------------
-fit_tan_g <- function(target, data, root = NULL, distributions = list(),
+# Learning CG TAN--------------------------------
+fit_tan_g <- function(target, data, root = NULL,
                       mutualInfoCond = NULL, all = F){
   
   if(is.null(target)){
@@ -87,57 +83,42 @@ fit_tan_g <- function(target, data, root = NULL, distributions = list(),
   if(!(target%in%colnames(data))){
     stop(paste0("Variable ", target, " must be a name of a variable of data"))
   }
-  # browser()
-  # Obtencion de la informacion mutua y distribuciones-------------------
-  # Obtencion de la estructura--------------------------------------
-  # hay variables discretas
+  # Structural learning--------------------------------------
+  # Compute the MI matrix
+  if(is.null(mutualInfoCond)){
+    mutualInfoCond = MI_tan_gauss(data,target)
+  }
+  # Is there a discrete variable?
   varPred = setdiff(colnames(data),target)
   pred_cont = sapply(varPred,function(variable){
     is.numeric(data[1,variable])
   })
   pred_cont = names(pred_cont)[which(pred_cont)]
   pred_disc = setdiff(varPred,pred_cont)
-  # Obtenemos si fuese necesario la raiz
-  if(is.null(root)||!(root%in%pred_disc)){
+  # browser()
+  # Compute the root
+  if(length(pred_disc)>0){
+    if(is.null(root)||!(root%in%pred_disc)){
+     root = fit_root(mutualInfoCond,pred_disc)
+    }
+  }else if(is.null(root)){
     root = fit_root(mutualInfoCond,pred_disc)
   }
   
-  # Obtenemos si fuese necesario la raiz
-  if(is.null(root)||!(root%in%pred_disc)){
-    root = fit_root(mutualInfoCond,pred_disc)
-  }
-  # browser()
-  adj_mat = fit_tan_structure(mutualInfoCond,target,root)
+  # Compute the adjacency matrix
+  adj_mat = fit_tan_structure(mutualInfoCond,target,root,pred_disc)
   dag = empty.graph(colnames(data))
   amat(dag)<-adj_mat
   bn = bn.fit(dag,data = data)
+  
   if(all){# Devolver informacion mutua y distribicones tambien
-    return(list(bn=bn,mutualInfoCond=mutualInfoCond,distributions=distributions))
+    return(list(bn=bn,mutualInfoCond=mutualInfoCond))
   }else{
     return(bn)
   }
 }
 
-## Condicional Gaussiano--------------------------------------------
-# X = varX
-# data = datos[,c(target,X)]
-mi_cond_gauss = function(X,target,data){
-  levels_target = levels(data[[target]])
-  varX = var(data[[X]])
-  varX_t = sapply(levels_target, function(i){
-    var(data[data[[target]]==i,X])
-  })
-  prob_target = as.vector(prop.table(table(data[[target]])))
-  return(1/2*(log(varX)-sum(log(varX_t)*prob_target)))
-}
-pVar_C = function(discVar_C,pC){
-  var = discVar_C$node
-  pVar_C = data.frame(C = pC[["C"]],
-                      mu = as.vector(discVar_C$coefficients[1,]),
-                      sd = as.vector(discVar_C$sd))
-
-  return(pVar_C)
-}
+## Mutual information functions--------------------------------------------
 
 cond_mi_cont = function(X,Y,target,data){
   levels_target = levels(data[[target]])
@@ -150,14 +131,18 @@ cond_mi_cont = function(X,Y,target,data){
 }
 
 cond_mi_cont_disc = function(X,Y,target,data){
-  browser()
+  # target or class variable distribution
   levels_target = levels(data[[target]])
   prob_target = as.vector(prop.table(table(data[[target]])))
+  # Feature variable distributions
+  ## Continuous
   varX_t = sapply(levels_target, function(i){
     var(data[data[[target]]==i,X])
   })
+  #Discrete
   levels_Y = levels(data[[Y]])
   prob_joint = prop.table(table(data[,c(target,Y)]))
+  # Compute Mutual information
   var_Y_T = c()
   i = levels_target[1]
   j = levels_Y[1]
@@ -169,43 +154,40 @@ cond_mi_cont_disc = function(X,Y,target,data){
                     )
     )
   }
-  # Posibles errores de redondeo o varianzas de 0
+  # Variances very smalls
   var_Y_T = ifelse(var_Y_T<10^-6,10^-6,var_Y_T)
   return(1/2*(sum(prob_target*log(varX_t))-sum(prob_joint*log(var_Y_T),na.rm = T)))
 }
 
-
+# Mutual information of dataset------------------
 MI_tan_gauss = function(data,target){
-  
-  # Información mutua entre discretas
   varPred = setdiff(colnames(data),target)
   MI = matrix(0,ncol = length(data)-1,nrow = length(data)-1,
               dimnames = list(varPred,varPred))
-  for(Y in varPred){
-    ff = function(X,Y,target,data){
+  
+  i = 1
+  j = 2
+  # Compute MI between each pair of features variables
+  for(i in 1:(length(varPred)-1)){
+    Y = varPred[i]
+    Y_numeric = is.numeric(data[[Y]])
+    for(j in (i+1):length(varPred)){
+      X = varPred[j]
       if(is.numeric(data[[X]])){
-        if(is.numeric(data[[Y]])){
-          return(cond_mi_cont(X,Y,target,data))
-        }else{
-          return(-1)
+        if(Y_numeric){
+          MI[X,Y] = cond_mi_cont(X,Y,target,data)
+          MI[Y,X] = MI[X,Y]
         }
       }else{
-        if(is.numeric(data[[Y]])){
-          return(cond_mi_cont_disc(Y,X,target,data))
+        if(Y_numeric){
+          MI[X,Y] = cond_mi_cont_disc(Y,X,target,data)
+          MI[Y,X] =  MI[X,Y] # CG asumptions
         }else{
-          return(infotheo::condinformation(data[[X]],data[[Y]],data[[target]]))
+          MI[X,Y] = infotheo::condinformation(data[[X]],data[[Y]],data[[target]])
+          MI[Y,X] = MI[X,Y]
         }
       }
     }
-    Mis = sapply(X = varPred, function(i){
-      if(i==Y){
-        return(0)
-      }else{
-        return(ff(i,Y,target,data[,c(i,Y,target)]))
-      }
-    })
-    
-    MI[names(Mis),Y] = Mis
   }
   return(MI)
 }
